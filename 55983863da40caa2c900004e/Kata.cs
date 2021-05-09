55db1d7d383c788c1d1d902eec579ca3cdaaae7c
Paul Osterhout <posterho@hotmail.com>
2021-05-09 17:40:33 -0400

    20210509_2

--- 55983863da40caa2c900004e/Kata.cs
+++ 55983863da40caa2c900004e/Kata.cs
@@ -5,7 +5,59 @@ namespace CodeWars.Kata_55983863da40caa2c900004e
 {
 	public class Kata
 	{
-		public static HashSet<string> GetPossibles(string digits)
+		public static long NextBiggerNumber(long n)
+		{
+			long next = -1;
+			List<int> digits = n.ToString().Select(x => int.Parse(x.ToString())).ToList();
+			int digitsLength = digits.Count;
+			for (int startDigit = digitsLength - 2; startDigit >= 0; startDigit--)
+			{
+				bool foundNumber = false;
+				foreach (string combination in GetCombinations(digits.GetRange(startDigit, digitsLength - startDigit)))
+				{
+					long possible = long.Parse(string.Concat(digits.GetRange(0, startDigit)) + combination);
+					if (possible > n)
+					{
+						foundNumber = true;
+						if ((next == -1) || (possible < next)) next = possible;
+					}
+				}
+				if (foundNumber) return next;
+			}
+			return next;
+		}
+
+		public static HashSet<string> GetCombinations(List<int> digits)
+		{
+			int digitsLength = digits.Count;
+			if (digitsLength == 2)
+			{
+				return new HashSet<string>
+				{
+					$"{digits[0]}{digits[1]}",
+					$"{digits[1]}{digits[0]}",
+				};
+			}
+			HashSet<string> combinations = new HashSet<string>();
+			foreach (int firstDigit in digits.Distinct())
+			{
+				List<int> innerDigits = digits.Distinct().OrderBy(x => x).ToList();
+				innerDigits.Remove(firstDigit);
+				foreach (string combination in GetCombinations(innerDigits))
+				{
+					if (!combinations.Contains(combination)) combinations.Add(firstDigit.ToString() + combination);
+				}
+			}
+			return combinations;
+		}
+
+		private static long Old(long n)
+		{
+			HashSet<long> numbers = GetPossibles(n.ToString()).Select(x => long.Parse(x.ToString())).ToHashSet();
+			return numbers.Where(x => x > n).Min();
+		}
+
+		private static HashSet<string> GetPossibles(string digits)
 		{
 			if (digits.Length == 1) return new HashSet<string> { digits };
 			HashSet<string> possibles = new HashSet<string>();
@@ -19,12 +71,6 @@ namespace CodeWars.Kata_55983863da40caa2c900004e
 				}
 			}
 			return possibles;
-		}
-
-		public static long NextBiggerNumber(long n)
-		{
-			HashSet<long> numbers = GetPossibles(n.ToString()).Select(x => long.Parse(x.ToString())).ToHashSet();
-			return numbers.Where(x => x > n).Min();
 		}
 	}
 }
--- 55983863da40caa2c900004e/UnitTests.cs
+++ 55983863da40caa2c900004e/UnitTests.cs
@@ -9,11 +9,19 @@ namespace CodeWars.Kata_55983863da40caa2c900004e
 		[Test]
 		public void Test1()
 		{
-			Console.WriteLine("****** Small Number");
+			Console.WriteLine("****** Small Number1");
 			Assert.AreEqual(21, Kata.NextBiggerNumber(12));
 			Assert.AreEqual(531, Kata.NextBiggerNumber(513));
 			Assert.AreEqual(2071, Kata.NextBiggerNumber(2017));
 			Assert.AreEqual(441, Kata.NextBiggerNumber(414));
+		}
+
+		[Test]
+		public void Test2()
+		{
+			Console.WriteLine("****** Small Number2");
+			Assert.AreEqual(2107, Kata.NextBiggerNumber(2071));
+			Assert.AreEqual(2170, Kata.NextBiggerNumber(2107));
 			Assert.AreEqual(414, Kata.NextBiggerNumber(144));
 		}
 
@@ -21,12 +29,6 @@ namespace CodeWars.Kata_55983863da40caa2c900004e
 		public void GetLargeNumber()
 		{
 			Assert.AreEqual(111111111111121, Kata.NextBiggerNumber(111111111111112));
-		}
-
-		[Test]
-		public void GetPossibles()
-		{
-			Assert.AreEqual(2, Kata.GetPossibles("12").Count);
 		}
 	}
 }
